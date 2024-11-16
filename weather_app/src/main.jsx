f3af98355ae2d31194f77805591341fa5b19ab3b
NIPIN1997 <133631101+NIPIN1997@users.noreply.github.com>
2024-11-16 18:31:44 +0530

    modified

--- weather_app/src/main.jsx
+++ weather_app/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Home } from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
